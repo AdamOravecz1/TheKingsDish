4af50a99990a634349c1b9e630f35f454074986b
AdamOravecz1 <oravecz.adam2004@gmail.com>
2024-11-05 14:18:10 +0100

    dungeon

--- Scenes/gate.gd
+++ Scenes/gate.gd
@@ -2,12 +2,13 @@
 extends Node2D
 
 @onready var interaction_area: InteractionArea = $InteractionArea
-@export_enum("forest", "castle") var level := "forest"
+@export_enum("forest", "castle", "dungeon") var level := "forest"
 var levels = {}
 func _ready():
 	if not Engine.is_editor_hint():
 		levels["forest"] = "res://Scenes/forest.tscn"
 		levels["castle"] = "res://Scenes/castle.tscn"
+		levels["dungeon"] = "res://Scenes/dungeon.tscn"
 
 	interaction_area.interact = Callable(self, "_enter")
 	
