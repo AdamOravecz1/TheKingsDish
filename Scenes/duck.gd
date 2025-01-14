71e8f996d9b7598eaa26c664dd9c4ebe21b39ebe
AdamOravecz1 <163161902+AdamOravecz1@users.noreply.github.com>
2025-01-14 23:23:17 +0100

    duck swim

--- Scenes/duck.gd
+++ Scenes/duck.gd
@@ -8,6 +8,8 @@ var speed_modifier := 1
 var can_move := true
 var fly := false
 var descend := false
+var swimming := false
+var floating := false
 @export var type := 0
 
 @onready var player = get_tree().get_first_node_in_group("Player")
@@ -23,11 +25,11 @@ var gravity_multiplier := 1
 func _ready():
 	if type == 0:
 		animation = $DuckMale
-		$DucFemale.hide()
+		$DuckFemale.hide()
 		$DuckMale.show()
 	elif type == 1:
-		animation = $DucFemale
-		$DucFemale.show()
+		animation = $DuckFemale
+		$DuckFemale.show()
 		$DuckMale.hide()
 	health = Global.animal_parameters["duck"]["health"]
 	interaction_area.interact = Callable(self, "_pickup")
@@ -43,22 +45,28 @@ func _process(delta):
 		if fly:
 			animation.play("flight")
 			velocity.y = -speed * speed_modifier
+		elif swimming:
+			velocity.y = 0
+
 		elif descend:
 			velocity.y = speed * speed_modifier
 			if is_on_floor():
 				descend = false
 				animation.play("walk")
 				speed_modifier = 1
+
 		velocity.x = x_direction * speed * speed_modifier
 		check_cliff()
 		check_wall()
 		check_player_distance()
 		animate()
 		move_and_slide()
-	if not fly and not descend:
+	if not fly and not descend and not swimming and not floating:
 		apply_gravity(delta)
 		if not is_on_floor():
 			move_and_slide()
+	if floating and not alive:
+		velocity.y = 0
 		
 func check_player_distance():
 	var distance_to_player = position.distance_to(player.global_position)
@@ -83,10 +91,11 @@ func apply_gravity(delta):
 	velocity.y = min(velocity.y, terminal_velocity)
 	
 func check_cliff():
-	if x_direction > 0 and not $FloorRays/Right.get_collider():
-		x_direction = -1
-	if x_direction < 0 and not $FloorRays/Left.get_collider():
-		x_direction = 1
+	if not fly and not descend and not swimming:
+		if x_direction > 0 and not $FloorRays/Right.get_collider():
+			x_direction = -1
+		if x_direction < 0 and not $FloorRays/Left.get_collider():
+			x_direction = 1
 
 func check_wall():
 	if x_direction > 0 and $WallRays/Right.is_colliding():
@@ -103,9 +112,34 @@ func trigger_death():
 		$InteractionArea.monitoring = true
 		alive = false
 		set_collision_layer_value(3, false)
+		set_collision_layer_value(6, true)
+
 
 
 func _on_flight_timeout():
 	if alive:
 		fly = false
 		descend = true
+		
+func swim():
+	floating = true
+	swimming = true
+	speed_modifier = 1
+	descend = false
+	if alive:
+		animation.play("swim")
+	
+func not_swim():
+	swimming = false
+
+func snap_to_surface(water_surface_y):
+
+	if not swimming:
+		return  # Avoid redundant snapping if already swimming
+
+	# Snap the duck to the water surface
+	global_position.y = water_surface_y + 60
+	velocity.y = 0  # Ensure no vertical movement
+	
+func top_of_water():
+	floating = false
--- Scenes/water_area.gd
+++ Scenes/water_area.gd
@@ -4,7 +4,12 @@ extends Area2D
 func _on_body_entered(body):
 	if "swim" in body:
 		body.swim()
-		
+	if "snap_to_surface" in body:
+		# Try to find the CollisionShape2D as the first child
+		var collision_shape = get_child(0)
+		if collision_shape and collision_shape is CollisionShape2D:
+			var water_surface_y = global_position.y - collision_shape.shape.extents.y
+			body.snap_to_surface(water_surface_y)
 func _on_body_exited(body):
 	if "not_swim" in body:
 		body.not_swim()
