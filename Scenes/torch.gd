218f3cc468a3ed6bb65127dca13121e955ae014e
AdamOravecz1 <163161902+AdamOravecz1@users.noreply.github.com>
2025-04-12 17:33:25 +0200

    torch types

---
+++ Scenes/torch.gd
@@ -0,0 +1,29 @@
+@tool
+extends Node2D
+
+@export_enum("Torch", "Candle", "Chandelier")
+var type: int = 0:
+	set(value):
+		type = value
+		_update_animation()
+
+@onready var animation := $AnimatedSprite2D
+@onready var light := $PointLight2D
+
+func _ready():
+	_update_animation()
+
+func _update_animation():
+	if not is_instance_valid(animation):
+		return
+	match type:
+		0:
+			animation.play("torch")
+		1:
+			animation.play("candle")
+			light.color = Color(1, 1, 1)
+		2:
+			animation.play("chandelier")
+			light.color = Color(1, 1, 1)
+			
+
