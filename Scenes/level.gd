c49f39428066dfa3e9c27111979cef6c1b585dce
AdamOravecz1 <163161902+AdamOravecz1@users.noreply.github.com>
2025-07-05 17:27:17 +0200

    music

--- Scenes/level.gd
+++ Scenes/level.gd
@@ -22,6 +22,7 @@ var can_open := true
 @onready var player = get_tree().get_first_node_in_group("Player")
 
 func _ready():
+	Music.play_level_music()
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	if Global.current_day == rain_day and self.name != "Dungeon":
 		$Sound/Rain.play()
---
+++ Scenes/music.gd
@@ -0,0 +1,15 @@
+extends AudioStreamPlayer
+
+var level_music = preload("res://Music/Forest_v3.wav")
+
+func _play_music(music = AudioStream):
+	bus = "Music" 
+	if stream == music:
+		return
+	stream = music
+	play()
+	
+func play_level_music():
+	_play_music(level_music)
+	
+	
